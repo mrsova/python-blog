356b187af769ae858b1f5ab0d124e0f8addc7ec3
webmaster <webmaster@gresso.ru>
2017-11-23 09:57:18 +0300

    add template

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.core.urlresolvers import reverse
 # Create your models here.
 
 class Post(models.Model):
@@ -10,3 +10,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolut_url(self):
+        return reverse("posts:detail", kwargs={"id":self.id})
+        # return "/posts/%s/" %(self.id)
--- posts/views.py
+++ posts/views.py
@@ -1,23 +1,29 @@
-from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
+from .forms import PostForm
 from .models import Post
 
 def post_create(request):
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        instanse = form.save(commit=False)
+        instanse.save()
+        messages.success(request, "Успешно создан")
+        return HttpResponseRedirect(instanse.get_absolut_url())
     context = {
-       "title": "Create" 
+        "form":form
     }
 
-    return render(request, "index.html", context)
+    return render(request, "post_form.html", context)
 
 def post_detail(request, id=None):
-    #instance = Post.objects.get(id=10)
     instance = get_object_or_404(Post, id=id)
     context = {       
        "instance": instance  
     }
-
     return render(request, "post_detail.html", context)
 
 def post_list(request):
@@ -26,26 +32,24 @@ def post_list(request):
         "object_list": queryset,
         "title": "List" 
     }
-    # if request.user.is_authenticated():    
-    #     context = {
-    #        "title": "my user List" 
-    #     }
-    # else:
-    #      context = {
-    #        "title": "List" 
-    #     }
-    return render(request, "index.html", context)
+    return render(request, "post_list.html", context)
 
-def post_update(request):
-    context = {
-        "title": "Update" 
+def post_update(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    form = PostForm(request.POST or None, instance=instance)
+    if form.is_valid():
+        instanse = form.save(commit=False)
+        instanse.save()       
+        messages.success(request, "Успешно Обновлен", extra_tags='some-tag')
+        return HttpResponseRedirect(instanse.get_absolut_url())
+    context = {       
+       "instance": instance,
+       "form":form  
     }
+    return render(request, "post_form.html", context)
 
-    return render(request, "index.html", context)
-
-def post_delete(request):
-    context = {
-        "title": "Delete" 
-    }
-    
-    return render(request, "index.html", context)
+def post_delete(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    instance.delete()
+    messages.success(request, "Успешно Удален")
+    return redirect("posts:list")
